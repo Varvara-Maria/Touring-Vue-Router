e7d49407874bbc6b6b528ae32a0576e896b5e047
maho1 <mariia.honchariuk@sobaco-betax.com>
2023-09-13 15:24:26 +0200

    receiving-url-parameters/building-pagination/nested-routes/redirect-_-alias/programmatic-navigation

---
+++ src/views/event/Register.vue
@@ -0,0 +1,18 @@
+<template>
+  <p>Register for the here</p>
+  <button @click="register">Register Me</button>
+</template>
+
+<script>
+export default {
+  props: ['event'],
+  methods: {
+    register() {
+      this.$router.push({
+        name: 'EventDetails'
+      })
+    }
+  }
+
+}
+</script>
