ac310692791d0aa9c98fbae95fba1a7a785e6c66
maho1 <mariia.honchariuk@sobaco-betax.com>
2023-09-14 08:55:23 +0200

    flash-messages on Submit or Click

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
-import { createApp } from 'vue'
+import { createApp, reactive } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+const GStore = reactive({ flashMessage: ''}) // create a global (reactive) object to share across multiple components
+
 createApp(App)
   .use(store)
   .use(router)
+  .provide('GStore', GStore) // make this object available for components in our app to use (inject)
   .mount('#app')
